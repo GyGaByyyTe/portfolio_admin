a52debd02f4b28ccdf87f4ce137a11a1c603c09b
GyGaByyyTe <gygabyyyte@gmail.com>
2018-04-01 18:57:04 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .admin
+    .header
+      app-header
+    .tabs
+      tabs
+    .content
+      router-view
+</template>
+
+<script>
+import header from './components/header';
+import tabs from './components/tabs';
+
+export default {
+  components: {
+    appHeader: header,
+    tabs
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.content {
+  position: relative;
+  background: url('~images/bg.jpg') center center / cover no-repeat;
+  padding: 2rem 1.5rem;
+
+  &:before {
+    content: ' ';
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    background-color: #ffffff;
+    opacity: 0.85;
+  }
+}
+</style>
---
+++ src/components/blog.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  div.blog
+    h2 Страница "Блог"
+    .blog-title Добавить запись
+    .blog-row
+      input(type='text' name='article-name' class='blog-input' placeholder="Название" required)
+    .blog-row      
+      input(type='text' name='article-date' class='blog-input' placeholder="Дата" required)  
+    .blog-row      
+      textarea(cols='30' rows='4' name='article-content' class='blog-text-area' placeholder="Содержание")   
+    button(class="blog-save" type="button" @click="saveArticle") Добавить      
+</template>
+<script>
+export default {
+  methods: {
+    saveArticle() {
+      console.log('save all to JSON');
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.blog {
+  position: relative;
+}
+.blog-row {
+  position: relative;
+  margin-top: 1.3rem;
+
+  input {
+    width: 80%;
+    max-width: 25rem;
+    padding: 0.5rem 1rem;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+  }
+
+  textarea {
+    padding: 0.5rem 1rem;
+    border: none;
+    border-radius: 5px;
+    resize: none;
+    overflow: hidden;
+    outline: none;
+    width: 100%;
+    max-width: 35rem;
+    height: 10rem;
+  }
+}
+.blog-save {
+  margin-top: 1.5rem;
+  width: 119px;
+  height: 46px;
+  background-color: $green;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  outline: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
